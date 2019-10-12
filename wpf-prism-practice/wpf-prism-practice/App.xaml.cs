0ab3c76fa5964c78e45b269fa483833d338f38fa
Go Maruyama <gomaru503@gmail.com>
2019-10-12 13:53:34 +0900

    Initial
    Initialize project

---
+++ wpf-prism-practice/wpf-prism-practice/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using wpf_prism_practice.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace wpf_prism_practice
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
